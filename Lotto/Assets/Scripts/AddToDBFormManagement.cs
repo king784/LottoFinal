0e7307b899cae922a11ae6df1020a35074f08d26
Janessa <paradox.mochi@gmail.com>
2019-07-25 09:55:51 +0300

    simple ui and validation for raffle settings

---
+++ Lotto/Assets/Scripts/AddToDBFormManagement.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AddToDBFormManagement : MonoBehaviour
+{
+    //add items
+    public TMP_InputField itemName;
+    public TMP_InputField description;
+    public TMP_InputField quantity;
+
+    //reaffle settings
+    public TMP_InputField totalTickets;
+    public TMP_InputField winningChance;
+    public TMP_InputField raffleName;
+
+    //errors
+    public TMP_Text chanceError;
+    public TMP_Text totalError;
+    public GameObject errorMessagePanel;
+
+    public void resetFields()
+    {
+        //add item to db form
+        itemName.text = "";
+        description.text = "";
+        quantity.text = "1";
+
+        //raffle settings form
+        raffleName.text = "";
+        totalTickets.text = "20";
+        winningChance.text = "50";
+    }
+
+    public void checkSettingsInput()
+    {
+        bool errorsExist = false;
+        int chancePercent;
+        bool winChanceWasNumber = int.TryParse(winningChance.text, out chancePercent);
+
+        if (winChanceWasNumber)
+        {
+            if (chancePercent < 0 || chancePercent > 100)
+            {
+                chanceError.text = "Voitto mahdollisuuden tulee olla väliltä 0-100.";
+                errorsExist = true;
+            }
+        }
+        else
+        {
+            chanceError.text = "Voitto mahdollisuus kenttään pitää laittaa numero väliltä 0-100.";
+            errorsExist = true;
+        }
+        if (winningChance.text == "")
+        {
+            chanceError.text = "Voitto mahdollisuus kenttä ei saa olla tyhjä.";
+            errorsExist = true;
+        }
+
+        int totalTicketsNum;
+        bool totalTicketsWasNumber = int.TryParse(totalTickets.text, out totalTicketsNum);
+
+        if (totalTicketsWasNumber)
+        {
+            if (totalTicketsNum < 10 || totalTicketsNum > 40)
+            {
+                totalError.text = "Pallojen määrä pitää olla enemmän kuin 10 ja vähemmän kuin 40";
+                errorsExist = true;
+            }
+        }
+        else
+        {
+            totalError.text = "Pallojen määrä tulee olla numero";
+        }
+        if (totalTickets.text == "")
+        {
+            totalError.text = "Pallojen määrä ei saa olla tyhjä.";
+            errorsExist = true;
+        }
+
+        if(errorsExist){
+            errorMessagePanel.SetActive(true);
+            return;
+        }else{// if no errors send info to database
+
+        }
+    }
+
+}
--- Lotto/Assets/Scripts/DB_ModificationUIController.cs
+++ Lotto/Assets/Scripts/DB_ModificationUIController.cs
@@ -6,45 +6,37 @@ public class DB_ModificationUIController : MonoBehaviour
 {
     public GameObject addToDBPanel;
     public GameObject removeFromDBPanel;
-
+    public GameObject raffleSettingsPanel;
+    public GameObject wrongInputPopup;
     public GameObject DBModificationMenu;
 
-    public GameObject removePopUp;
-    public GameObject AddPopUp;
     private void Start()
     {
         closeAllPanels();
     }
     public void openAddToDB()
     {
-        removeFromDBPanel.SetActive(false);
         DBModificationMenu.SetActive(false);
         addToDBPanel.SetActive(true);
     }
 
     public void openRemoveFromDB()
     {
-        addToDBPanel.SetActive(false);
         DBModificationMenu.SetActive(false);
         removeFromDBPanel.SetActive(true);
     }
 
-    public void openRemovePopUP()
-    {
-        removePopUp.SetActive(true);
-    }
-
-    public void openAddPopUp()
-    {
-        AddPopUp.SetActive(true);
+    public void openRaffleSettings(){
+        DBModificationMenu.SetActive(false);
+        raffleSettingsPanel.SetActive(true);
     }
 
     public void closeAllPanels()
     {
         removeFromDBPanel.SetActive(false);
-        removePopUp.SetActive(false);
-        AddPopUp.SetActive(false);
+        raffleSettingsPanel.SetActive(false);
         addToDBPanel.SetActive(false);
+        wrongInputPopup.SetActive(false);
         DBModificationMenu.SetActive(true);
     }
 
