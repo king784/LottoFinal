2d7c7c6c4298e93cee4421398b936162a033d693
king784 <teemu.turku@pp2.inet.fi>
2019-07-25 11:49:31 +0300

    list deletes

---
+++ Lotto/Assets/Scripts/CallRemoveFromDB.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CallRemoveFromDB : MonoBehaviour
+{
+    public void RemoveFromDB()
+    {
+        string tempID = transform.parent.GetChild(0).GetComponent<TextMeshProUGUI>().text;
+        Debug.Log(tempID);
+        Item tempItem = FindObjectOfType<ItemManager>().allItems.Find(newItem => newItem.itemId.ToString() == tempID);
+        Debug.Log(tempItem.itemId.ToString() + " " + tempItem.itemName + " " + tempItem.description + " ");
+        FindObjectOfType<ItemManager>().winItem = tempItem;
+        FindObjectOfType<ItemManager>().DeleteFromDB();
+        Destroy(transform.parent.gameObject);
+    }
+}
