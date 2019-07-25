b78b633f22f9cb3b76c8d360d10fa252e106b6ed
RikuH <45202322+RikuH@users.noreply.github.com>
2019-07-25 09:37:07 +0300

    alkujuttui

---
+++ Lotto/Assets/winOrLose/startGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startGame : MonoBehaviour
+{
+
+    Animator anim;
+    public GameObject paneeli;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void startGameButton(){
+        Debug.Log("Alkaa");
+        anim.SetBool("startade", true);
+        StartCoroutine(toTheGame());
+    }
+
+    IEnumerator toTheGame(){
+        yield return new WaitForSeconds(0.5f);
+        anim.SetBool("startade", true);
+        paneeli.SetActive(false);
+        Debug.Log("Valmis");
+    }
+}
