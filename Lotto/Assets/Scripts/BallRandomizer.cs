244f1a71310c566efe34cb1e767510a317903d3c
king784 <teemu.turku@pp2.inet.fi>
2019-10-04 14:55:23 +0300

    ffs

---
+++ Lotto/Assets/Scripts/BallRandomizer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallRandomizer : MonoBehaviour
+{
+    [SerializeField]
+    float minForce = 15.0f; //3
+    [SerializeField]
+    float maxForce = 20.0f; //15
+
+    public Vector3 randomizedVec;
+
+    void Start()
+    {
+        GetRandomNumber();
+        gameObject.GetComponent<Rigidbody>().AddForce(randomizedVec, ForceMode.Impulse);
+    }
+
+    void GetRandomNumber()
+    {
+        randomizedVec = new Vector3(
+            Random.Range(minForce, maxForce),
+            Random.Range(minForce, maxForce),
+            Random.Range(minForce, maxForce)
+        );
+    }
+}
